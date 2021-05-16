b186037752eda5cc79da02cdae446fc75b5acb0b
UBatir <urazbaaev.br@gmail.com>
2021-05-16 21:13:53 +0500

    createdAt was added

--- app/src/main/java/com/example/sqlitetofirebase/MainActivity.kt
+++ app/src/main/java/com/example/sqlitetofirebase/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.sqlitetofirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,11 +21,22 @@ class MainActivity : AppCompatActivity() {
         data = dao.getAllData()
         btnOK.setOnClickListener {
             val map:MutableMap<String,Any> = mutableMapOf()
-            data.forEach{ it ->
+            data.forEach{ str ->
+                var soraw = str.soraw.replace("<p><br></p>", "")
+                soraw = soraw.removePrefix("<i style=\"color:#900a0a\">Сораў: </i>")
+                soraw = soraw.replace("<p>", "")
+                soraw = soraw.replace("<o:p></o:p>", "")
+                soraw = soraw.replace("<i><span style=\"color: rgb(227, 108, 9);\">Сораў:</span></i><span style=\"color: rgb(227, 108, 9);\"></span>", "")
+                soraw = soraw.replace("</p>", "<br><br>")
+                soraw = soraw.removeSuffix("<br><br>")
                 map["id"] = UUID.randomUUID().toString()
                 map["categoryId"]="lJSsuaj1p7uCW5cSw6e7"
-                map["soraw"] = it.soraw.toString()
-                map["juwap"] = it.juwap.toString()
+                val sdf = SimpleDateFormat("dd.MM.yyyy")
+                val date:Date=sdf.parse(sdf.format(Calendar.getInstance().time).toString())
+                map["createdAt"]=(date.time)/1000
+                map["soraw"] = soraw
+                map["juwap"] = str.juwap
+                Log.d("soraw", "$soraw \n ${str.juwap}")
                 db.collection("questions").document(map["id"].toString()).set(map)
                         .addOnSuccessListener {
                             Toast.makeText(this, "Otlichno", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/sqlitetofirebase/SorawJuwapClass.kt
+++ app/src/main/java/com/example/sqlitetofirebase/SorawJuwapClass.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName="sorawjuwap")
 data class SorawJuwapClass(
     @PrimaryKey
-    @ColumnInfo(name= "id") val id:Int?,
-    @ColumnInfo(name= "soraw") val soraw:String?,
-    @ColumnInfo(name= "juwap") val juwap:String?,
+    @ColumnInfo(name= "id") val id:Int,
+    @ColumnInfo(name= "soraw") val soraw:String,
+    @ColumnInfo(name= "juwap") val juwap:String,
 )
